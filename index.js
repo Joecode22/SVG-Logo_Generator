969d582f52375df1363b2a7e55b103ac7c65c48d
josik <joe.code.mckinney@gmail.com>
2023-05-04 21:04:01 -0500

    initial boilerplate

---
+++ index.js
@@ -0,0 +1,10 @@
+//Require the inquirer package
+const inquirer = require("inquirer");
+//Require fs
+const fs = require("fs");
+//Require our classes from shapes.js
+const { Circle, Square, Triangle } = require("./lib/shapes.js");
+
+createSVG = function(text, ){
+
+}
